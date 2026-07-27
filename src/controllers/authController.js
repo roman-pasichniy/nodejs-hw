5fe01a60868ba64aab363f9c8f3afbcfda9ea0d0
Roman Pasichniy <roman.pasichniy@gmail.com>
2026-07-27 21:29:03 +0300

    fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -118,6 +118,7 @@ export const requestResetEmail = async (req, res, next) => {
     });
 
     await sendEmail({
+      from: process.env.SMTP_FROM,
       to: user.email,
       subject: 'Password reset request',
       html,
